f05a6d7df4001fd1bf97b099198390ada1279ef9
kevinzepwa <zepwakevin8@gmail.com>
2021-11-20 15:05:05 +0300

    adjustment

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,12 +5,10 @@ class About extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const name = this.props.data.name;
+    
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const phone = this.props.data.phone;
-    const email = this.props.data.email;
-    const resumeDownload = this.props.data.resumedownload;
+    
 
     return (
       <section id="about">
